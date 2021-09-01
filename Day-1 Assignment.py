5e1c8312392a70bd0574a17cdba79480ff0865dc
Thippeswamy.j@invibes.in <thippeswamyj020@gmail.com>
2021-09-01 10:23:48 +0530

    Pyton class materials

---
+++ Day-1 Assignment.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
+a=input('Please Enter your name:')
+b=input('Please Enter your Age:')
+c=input('Please Enter your Designation:')
+print('Welcome',a,'of Age',b, 'for the new Role of',c)
+
+
+# In[5]:
+
+
+a=eval(input())
+b=eval(input())
+Add=a+b
+Sub=a-b
+Mult=a*b
+Div=a/b
+Pow=a**b
+fD=a//b
+print(Add)
+print(Sub)
+print(Mult)
+print(Div)
+print(Pow)
+print(fD)
+
+
+# In[20]:
+
+
+a=eval(input('Enter first number: '))
+b=eval(input('Enter Second number: '))
+if a>b:
+    print("a is Greater than b")
+elif a<b:
+    print('a is smaller than b')
+else:
+    print('a is equal to b')
+        
+       
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
